54ffce7e905ecdf31583cc62a0b8943f2f425ccb
Maciej Chudzinski <mchudz97@gmail.com>
2020-04-26 11:00:15 +0200

    setter test  repair + method

--- src/main/java/proj2/entities/Product.java
+++ src/main/java/proj2/entities/Product.java
@@ -2,6 +2,23 @@ package proj2.entities;
 
 public class Product {
 
+    private String name;
 
+
+
+    public void setName(String name){
+
+
+
+
+        if(name == null || !name.matches("([A-Z][a-z]{1,244}[ ]?)+")){
+
+            throw new IllegalArgumentException("Invalid product name!");
+
+        }
+
+        this.name = name;
+
+    }
 
 }
--- src/test/java/proj2/enitites/tests/mockito/ProductTest.java
+++ src/test/java/proj2/enitites/tests/mockito/ProductTest.java
@@ -1,5 +1,6 @@
 package proj2.enitites.tests.mockito;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,7 +16,12 @@ public class ProductTest {
 
 
 
+    @BeforeEach
+    public void reset(){
 
+        product = new Product();
+
+    }
 
     private String nullizier(String nullexample){
 
@@ -32,10 +38,10 @@ public class ProductTest {
 
     @ParameterizedTest(name = "{index} Parameter {0} throws argument exception")
     @DisplayName("Unallowed name will throw exception")
-    @CsvSource({"", "null", " ", "    ", "Ala  makota", "A", " Ala", "Product @$%!"})
+    @CsvSource({"''", "null", "' '", "'    '", "Ala  makota", "A", "' Ala'", "Product @$%!"})
     public void unallowedNameTest(String val){
 
-        assertThatThrownBy(() -> product.setName(val)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> {product.setName(nullizier(val));}).isInstanceOf(IllegalArgumentException.class);
 
     }
 
