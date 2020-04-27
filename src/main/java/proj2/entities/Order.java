2b8aea88f4812790b5ae2e385b4dfc313ef09171
Maciej Chudzinski <mchudz97@gmail.com>
2020-04-28 00:49:05 +0200

    setter client id added

--- src/main/java/proj2/entities/Order.java
+++ src/main/java/proj2/entities/Order.java
@@ -3,17 +3,30 @@ package proj2.entities;
 public class Order {
 
     private int id;
+    private int clientId;
 
 
     public void setId(int id){
 
-        if(id<0){
+        if(id < 0){
 
             throw new IllegalArgumentException("Id must be positive!");
 
         }
 
         this.id = id;
+
+    }
+
+    public void setClientId(int id){
+
+        if(id < 0){
+
+            throw new IllegalArgumentException("Id must be positive!");
+
+        }
+
+        this.clientId = id;
 
     }
 
